f99dec416d87d2f5072a4623d15548b811a9ff63
CH16-41 <434644399@qq.com>
2020-04-29 18:20:32 +0800

    添加admin页面中对模型的支持“

--- Book_App/admin.py
+++ Book_App/admin.py
@@ -1,3 +1,13 @@
+# Register your models here.
+
 from django.contrib import admin
 
-# Register your models here.
+
+from Account_App.models import User
+from Book_App.models import Seat
+from Book_App.models import Reservation
+
+
+admin.site.register(User)
+admin.site.register(Seat)
+admin.site.register(Reservation)
