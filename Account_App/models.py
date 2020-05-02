c93af22a8e81fe7503aee86d3952eeb51f6c0dd1
WuJiahao <2121866416@qq.com>
2020-05-02 19:53:12 +0800

    个人中心以及accoun_app的修改

--- Account_App/models.py
+++ Account_App/models.py
@@ -4,8 +4,14 @@ from django.contrib.auth.models import User
 # 用户模型初步创建
 class UserProf(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    icon = models.ImageField(upload_to='icon', default='static/image/default_icon.jpg')  # 头像(有默认头像)
+    icon = models.ImageField(upload_to='icon', default='static/image/zhangzhe.jpg')  # 头像(有默认头像)
     credit = models.PositiveIntegerField(default=5)  # 信誉度
+    introduction = models.TextField(default="个人简介")  # 个人简介
+    name = models.CharField(default="user", max_length=20)  # 姓名
+    student_number = models.CharField(max_length=12)  # 学号
+    mailbox = models.EmailField(default="user@email.com")  # 邮箱
+    phone_number = models.CharField(max_length=11)  # 电话号码
+    record_id = models.TextField(editable=False)  # 预约记录
 
     class Meta:
         db_table = 'userprof'
--- Center_App/views.py
+++ Center_App/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from Account_App.models import UserProf
 from django.http import HttpResponse
 
 
@@ -7,4 +8,18 @@ def homepage(request):
 
 
 def personal_center(request):
-    return render(request, 'main/personal_center.html')
+    user_id = request.session.get('user_id')
+    userprof = UserProf.objects.get(pk=1)
+    data = {
+        'icon': '../../static/uploads/' + userprof.icon.url,
+        'name': userprof.name,
+        'student_number': userprof.student_number,
+        'phone_number': userprof.phone_number,
+        'mailbox': userprof.mailbox,
+        'introduction': userprof.introduction,
+        'record_id_new': None,  # 待完成
+        # 记录id（待完成）
+        'credit': userprof.credit
+        # 不良记录id（待完成）
+    }
+    return render(request, 'main/personal_center.html', context=data)
