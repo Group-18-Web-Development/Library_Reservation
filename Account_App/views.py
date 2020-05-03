fe6f28ac00c804777c929c5748f2297ed83e5ab8
EmisonLu <lzh1633856298@gmail.com>
2020-05-03 23:27:15 +0800

    部分调整

--- Account_App/views.py
+++ Account_App/views.py
@@ -10,47 +10,6 @@ from Book_App.models import Reservation, Seat
 import random
 
 
-def add_data(request):
-    # # 添加用户数据
-    # for i in range(10):
-    #     user = User()
-    #     user.username = 'Jason' + str(i)
-    #     user.password = '00000' + str(i)
-    #     user.email = '123456@gmail.com'
-    #     user.save()
-
-    # 添加安静区座位
-    # for i in range(20):
-    #     seat = Seat()
-    #     seat.is_quiet_area = True
-    #     seat.floor = 5
-    #     seat.area = 'C'
-    #     seat.table_type = random.choice([1, 2, 4])
-    #     seat.has_power = random.choice([True, False, False, False])
-    #     seat.table_position_quiet = seat.area + str(seat.floor * 100) + '-' + str(i+1)
-    #     seat.save()
-
-    # 添加非安静区座位
-    # for s in range(5):
-    #     for i in range(30):
-    #         seat = Seat()
-    #         seat.is_quiet_area = False
-    #         seat.floor = s+1
-    #         seat.table_type = random.choice([1, 2, 4])
-    #         seat.table_position_noisy = 'F' + str(seat.floor) + '-' + str(i + 1)
-    #         seat.save()
-
-    # 添加预约记录
-    # for i in range(9):
-    #     record = Reservation()
-    #     record.account_id = i + 1
-    #     record.seat_id = random.randrange(50) + 1
-    #     record.time_id = random.choice([1, 2, 3])
-    #     record.save()
-
-    return HttpResponse('用户、座位添加成功')
-
-
 def user_login(request):
     if request.method == "POST":
         login_form = LoginForm(request.POST)
@@ -98,3 +57,68 @@ def register(request):
         userpro_form = UserProfForm()
 
         return render(request, "Account_App/register.html", {"form": user_form, "pro": userpro_form})
+
+
+def add_data(request):
+    # # 添加用户数据
+    # for i in range(10):
+    #     user = User()
+    #     user.username = 'Jason' + str(i)
+    #     user.password = '00000' + str(i)
+    #     user.email = '123456@gmail.com'
+    #     user.save()
+
+    # 添加安静区座位
+    # for j in range(5):
+    #     for i in range(20):
+    #         seat = Seat()
+    #         seat.is_quiet_area = True
+    #         seat.floor = j + 1
+    #         seat.area = 'A'
+    #         seat.table_type = random.choice([1, 2, 4])
+    #         seat.has_power = random.choice([True, False, False, False])
+    #         seat.table_position_quiet = seat.area + str(seat.floor * 100) + '-' + str(i+1)
+    #         seat.save()
+    #
+    # for j in range(5):
+    #     for i in range(20):
+    #         seat = Seat()
+    #         seat.is_quiet_area = True
+    #         seat.floor = j + 1
+    #         seat.area = 'B'
+    #         seat.table_type = random.choice([1, 2, 4])
+    #         seat.has_power = random.choice([True, False, False, False])
+    #         seat.table_position_quiet = seat.area + str(seat.floor * 100) + '-' + str(i+1)
+    #         seat.save()
+    #
+    # for j in range(5):
+    #     for i in range(20):
+    #         seat = Seat()
+    #         seat.is_quiet_area = True
+    #         seat.floor = j + 1
+    #         seat.area = 'C'
+    #         seat.table_type = random.choice([1, 2, 4])
+    #         seat.has_power = random.choice([True, False, False, False])
+    #         seat.table_position_quiet = seat.area + str(seat.floor * 100) + '-' + str(i+1)
+    #         seat.save()
+
+    # 添加非安静区座位
+    # for s in range(5):
+    #     for i in range(30):
+    #         seat = Seat()
+    #         seat.is_quiet_area = False
+    #         seat.floor = s+1
+    #         seat.table_type = random.choice([1, 2, 4])
+    #         seat.table_position_noisy = 'F' + str(seat.floor) + '-' + str(i + 1)
+    #         seat.save()
+
+    # 添加预约记录
+    # for i in range(9):
+    #     record = Reservation()
+    #     record.account_id = i + 1
+    #     record.seat_id = random.randrange(50) + 1
+    #     record.time_id = random.choice([1, 2, 3])
+    #     record.save()
+
+    return HttpResponse('用户、座位添加成功')
+
