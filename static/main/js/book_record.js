7d92666b7938d01edd0841132b5cd8f41a80cb76
Aray <62073294+aray-s@users.noreply.github.com>
2020-05-21 20:06:27 +0600

    修改取消预约confirm的style

---
+++ static/main/js/book_record.js
@@ -0,0 +1,33 @@
+function Confirm(title, msg, $true, $false, $link) {
+        var $content =  "<div class='dialog-ovelay'>" +
+                        "<div class='dialog'><header>" +
+                         " <h3> " + title + " </h3> " +
+                         "<i class='fa fa-close'></i>" +
+                     "</header>" +
+                     "<div class='dialog-msg'>" +
+                         " <p> " + msg + " </p> " +
+                     "</div>" +
+                     "<footer>" +
+                         "<div class='controls'>" +
+                             " <button class='button button-danger doAction'>" + $true + "</button> " +
+                             " <button class='button button-default cancelAction'>" + $false + "</button> " +
+                         "</div>" +
+                     "</footer>" +
+                  "</div>" +
+                "</div>";
+         $('body').prepend($content);
+      $('.doAction').click(function () {
+        window.open($link, "_blank");
+        $(this).parents('.dialog-ovelay').fadeOut(500, function () {
+          $(this).remove();
+        });
+      });
+$('.cancelAction, .fa-close').click(function () {
+        $(this).parents('.dialog-ovelay').fadeOut(500, function () {
+          $(this).remove();
+        });
+      });
+   }
+$('a.CancelBooking').click(function () {
+        Confirm('你在取消预约', '你确定吗？', 'Yes', 'Cancel',href="{% url 'book:book_cancel' reservation_id=record.id %}");
+    });
