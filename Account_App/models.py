0a3eb9f2c73c8025bf1c0d06f671ea9bb1b788be
EmisonLu <lzh1633856298@gmail.com>
2020-05-28 22:52:34 +0800

    总体调整1.0

--- Account_App/models.py
+++ Account_App/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class UserProf(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    icon = models.ImageField(upload_to='icon', default='static/image/zhangzhe.jpg')  # 头像(有默认头像)
+    icon = models.ImageField(upload_to='icon', default='icon/default_icon.jpg')  # 头像(有默认头像)
     credit = models.PositiveIntegerField(default=5)  # 信誉度
     delete_times = models.PositiveIntegerField(default=0)
     introduction = models.TextField(default="个人简介")  # 个人简介
--- Center_App/views.py
+++ Center_App/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -16,6 +17,7 @@ def personal_center(request):
         return redirect(reverse('account:user_login'))
 
     user_id = request.session.get('user_id')
+    user = User.objects.get(id=user_id)
     userprof = UserProf.objects.get(user_id=user_id)
 
     if request.method == 'POST':
@@ -23,14 +25,15 @@ def personal_center(request):
         userprof.icon = icon
         userprof.save()
 
+    msg = ""
+    if userprof.credit == 0:
+        msg = "您的信誉分过低，无法继续预约，请联系管理员！"
     data = {
         'icon': '../../static/uploads/' + userprof.icon.url,
-        'name': userprof.name,
-        'student_number': userprof.student_number,
+        'name': user.username,
         'phone_number': userprof.phone_number,
         'mailbox': userprof.user.email,
-        'introduction': userprof.introduction,
-        'record_id_new': None,  # 待完成
-        'credit': userprof.credit
+        'credit': userprof.credit,
+        'msg': msg,
     }
     return render(request, 'main/personal_center.html', context=data)
